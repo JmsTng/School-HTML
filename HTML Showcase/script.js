afc800e8a8f9e1d41dc101d44d4fc00187714860
JmsTng <james.ftung@gmail.com>
2022-10-09 18:29:40 -0400

    mail system better

--- HTML Showcase/script.js
+++ HTML Showcase/script.js
@@ -5,4 +5,5 @@ let toggle = () => {
 
     document.querySelector(".outgoing").style.display = t ? "none" : "initial";
     document.querySelector(".incoming").style.display = t ? "initial" : "none";
+    document.querySelector("#tog").textContent = t ? "Send me an email instead?" : "Subscribe to my newsletter instead?"
 }
